69a97234f3485e67e88adaf9249273b533ab9883
Robert Schuster <rschus@tarub.tarent.buero>
2010-10-22 13:43:51 +0200

    * fixed path for 'cat'

--- bug20/calibrate.sh
+++ bug20/calibrate.sh
@@ -2,5 +2,5 @@
 # TouchScreen calibration helper
 
 # poor man's if [ ! -f
-cat /data/system/tslib/pointercal > /dev/null 2> /dev/null ||
+/system/bin/cat /data/system/tslib/pointercal > /dev/null 2> /dev/null ||
   /system/bin/ts_calibrate nofork
