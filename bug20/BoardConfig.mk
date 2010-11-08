1bdfbed473aa5b9b7a0e9d99234fb8d40f7ee387
Robert Schuster <rschus@tarub.tarent.buero>
2010-11-08 14:23:45 +0100

    * added Wifi defines, sd8686 (SDIO) firmware and Marvell license  * added rules to copy firmware and license to image

--- bug20/BoardConfig.mk
+++ bug20/BoardConfig.mk
@@ -1,7 +1,11 @@
 # Definitions for the Bug20 board
 
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
+TARGET_NO_KERNEL := false
+
+TARGET_PREBUILT_KERNEL := 
+TARGET_KERNEL_DIR := kernel
+TARGET_KERNEL_CONFIG := omap3_blandroid_defconfig
 
 TARGET_PROVIDES_INIT_RC := true
 
@@ -21,6 +25,16 @@ BUILD_WITH_ALSA_UTILS := true
 
 USE_CAMERA_STUB := true
 
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION := VER_0_6_X
+BOARD_WLAN_DEVICE := libertas
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/libertas_sdio.ko"
+WIFI_DRIVER_FW_STA_PATH := "/system/etc/firmware/sd8686.bin"
+#WIFI_DRIVER_FW_AP_PATH := "/system/etc/firmware/sd8686.bin"
+WIFI_DRIVER_MODULE_ARG := "fw_name=/system/etc/firmware/sd8686.bin"
+WIFI_DRIVER_MODULE_NAME := "libertas_sdio"
+
 PRODUCT_COPY_FILES += \
 	device/buglabs/bug20/vold.conf:system/etc/vold.conf \
 	device/buglabs/bug20/vold.fstab:system/etc/vold.fstab \
@@ -30,5 +44,9 @@ PRODUCT_COPY_FILES += \
 	device/buglabs/bug20/ts.env:system/etc/ts.env \
 	device/buglabs/bug20/calibrate.sh:system/bin/calibrate.sh \
 	device/buglabs/bug20/init.rc:root/init.rc
+
+PRODUCT_COPY_FILES += \
+	device/buglabs/bug20/sd8686.bin:system/etc/firmware/sd8686.bin \
+	device/buglabs/bug20/Marvell-Licence.txt:system/etc/firmware/Marvell-Licence.txt
 
 include frameworks/base/data/sounds/AudioPackage2.mk
